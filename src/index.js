d1cf230416c0fa0fbc4df00a19af607744ae567b
vijayvij <vhat007@gmail.com>
2018-07-02 15:20:48 +0530

    Event creating

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app/layout/App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(<App />, rootEl);
 };
 
 if (module.hot) {
-  module.hot.accept("./app/layout/App", () => {
+  module.hot.accept('./app/layout/App', () => {
     setTimeout(render);
   });
 }
